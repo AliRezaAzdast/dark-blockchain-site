71d6cd84688de980ebffa3e542749c2fb2560d11
AliRezaAzdast <151525801+AliRezaAzdast@users.noreply.github.com>
2025-08-18 09:57:12 +0330

    it took me all night to add this animation specily hexagon animation it show it has error ever tho it work fin ill work on it after reast

--- src/components/Cricle.tsx
+++ src/components/Cricle.tsx
@@ -1,11 +1,22 @@
 import type { ComponentPropsWithoutRef } from "react"
 import { twMerge } from "tailwind-merge"
+import { motion } from "motion/react"
 
 
-function Cricle(props: ComponentPropsWithoutRef<'div'>) {
-    const {className, children} = props
+function Cricle(props: ComponentPropsWithoutRef<'div'> & { animate?: boolean, reverse?: boolean, speed?: number }) {
+    const { className, children, animate = false, reverse= false, speed= 15 } = props
     return (
-        <div className={twMerge('bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10', className)}>
+        <div className={twMerge('relative bg-zinc-900 size-[240px] inline-flex items-center justify-center rounded-full', className)}>
+            <motion.div
+                animate={animate && {
+                    rotate: reverse ? -360 : 360,
+                }}
+                transition={{
+                    ease: 'linear',
+                    duration: speed,
+                    repeat: Infinity
+                }}
+                className={twMerge("absolute inset-0 rounded-full outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 ", animate  && 'border-[6px] border-transparent border-t-fuchsia-500/30')}></motion.div>
             {children}
         </div>
     )
--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,9 +1,46 @@
+import { motion, useScroll, useTransform } from "motion/react"
+import { useRef } from "react"
 import Cricle from "../components/Cricle"
 import CutCornerButton from "../components/CutCornerButton"
 import Hexagon from "../components/Hexagon"
 
 
 function HeroSection() {
+  const icosahedronRef = useRef(null)
+  const cubeRef = useRef(null)
+  const torusRef = useRef(null)
+  const cuboidRef = useRef(null)
+
+
+  // torus animation
+  const { scrollYProgress: cuboidScrollYProgress } = useScroll({
+    target: cuboidRef,
+    offset: ['start end', 'end start']
+  })
+  const cuboidRotate = useTransform(cuboidScrollYProgress, [0, 1], [20, -20])
+
+
+  // torus animation
+  const { scrollYProgress: torusScrollYProgress } = useScroll({
+    target: torusRef,
+    offset: ['start end', 'end start']
+  })
+  const torusRotate = useTransform(torusScrollYProgress, [0, 1], [20, -20])
+
+  // cube animation
+  const { scrollYProgress: cubeScrollYProgress } = useScroll({
+    target: cubeRef,
+    offset: ['start end', 'end start']
+  })
+  const cubeRotate = useTransform(cubeScrollYProgress, [0, 1], [100, -45])
+
+  // icosahedron animation
+  const { scrollYProgress } = useScroll({
+    target: icosahedronRef,
+    offset: ['start end', 'end start']
+  })
+  const icosahedronRotate = useTransform(scrollYProgress, [0, 1], [30, -45])
+
   return (
     <section className="py-24 md:py-52 overflow-clip">
       <div className="my-container">
@@ -16,34 +53,47 @@ function HeroSection() {
         <div className="flex justify-center">
           <div className="inline-flex mt-24 relative z-0">
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <Hexagon className="size-[1100px]" />
+              <Hexagon className="size-[1100px]" size={1100} speed={25}/>
             </div>
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <Hexagon className="size-[1800px]" />
+              <Hexagon className="size-[1800px]" size={1800} reverse={true}/>
             </div>
 
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <Cricle className="absolute left-[200px] -top-[900px]">
-                <img src="./assets/images/cube.png" alt="cube 3D image" className="size-[140px]" />
+              <Cricle className="absolute left-[200px] -top-[900px]" animate={true}>
+                <motion.img ref={cubeRef} style={{ rotate: cubeRotate }}  src="./assets/images/cube.png" alt="cube 3D image" className="size-[140px]" />
               </Cricle>
             </div>
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <Cricle className="absolute left-[200px] top-[270px]">
-                <img src="./assets/images/cuboid.png" alt="Cuboid 3D image" className="size-[140px]" />
+              <Cricle className="absolute left-[200px] top-[270px]" animate={true} reverse={true} speed={20}>
+                <motion.img ref={cuboidRef} style={{rotate:cuboidRotate}} src="./assets/images/cuboid.png" alt="Cuboid 3D image" className="size-[140px]" />
               </Cricle>
             </div>
             <div className="absolute top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2">
-              <Cricle className="absolute -left-[600px] -top-[80px]">
-                <img src="./assets/images/torus.png" alt="Torus 3D image" className="size-[140px]" />
+              <Cricle className="absolute -left-[600px] -top-[80px]" animate={true} speed={25}>
+                <motion.img ref={torusRef} style={{ rotate: torusRotate }} src="./assets/images/torus.png" alt="Torus 3D image" className="size-[140px]" />
               </Cricle>
             </div>
-            <img src="./assets/images/icosahedron.png" className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]" />
-            <img src="./assets/images/icosahedron.png" alt="Icosahedron 3D Image" className="w-[500px]" />
+            <motion.div className="inline-flex" style={{ rotate: icosahedronRotate }} ref={icosahedronRef}>
+              <img src="./assets/images/icosahedron.png" className="absolute w-[calc(100%+100px)] max-w-none -z-10 top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 saturate-[10%] brightness-[4%] hue-rotate-[240deg]" />
+              <img src="./assets/images/icosahedron.png" alt="Icosahedron 3D Image" className="w-[500px]" />
+            </motion.div>
           </div>
         </div>
         <div className="flex items-center flex-col mt-40 md:mt-80 gap-4">
           <div className="h-10 w-5 outline-[6px] outline-fuchsia-500/10 inline-flex justify-center pt-2 rounded-full">
-            <div className="h-3 w-1 bg-fuchsia-500 rounded-full"></div>
+            <motion.div 
+              animate={{
+                translateY: 12,
+                opacity: 0.2,
+              }}
+              transition={{
+                duration:2,
+                ease: 'linear',
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+            className="h-3 w-1 bg-fuchsia-500 rounded-full"></motion.div>
           </div>
           <p className="uppercase text-zinc-500 font-extrabold tracking-wider">Scroll to learn more</p>
         </div>
